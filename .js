52f9c9fcb7a3878f4ea826451853e7a3f4fcf41e
Linhong Zhou <linhong.zhou@mihsnyc.org>
2018-01-09 23:31:25 +0000

    Finish Project

---
+++ .js
@@ -0,0 +1,23 @@
+$('document').ready(function(){
+    function piglatinizer (string){
+        var split = string.split('');
+        var text = "";
+        if( split[0] === 'a' || split[0] === 'e'|| split[0] === 'i' || split[0] === 'o' || split [0] === 'u'){
+            for( let i = 0; i < split.length; i++){
+                text = text + split[i];
+            }
+            text = text + 'yay';
+        } else {
+            for( let i = 1; i <split.length; i++){
+                text = text + split[i];
+            }
+            text = text + split[0];
+            text = text + 'ay';
+        }
+        console.log(text);
+    }
+    $('#button').click(function(){
+        var originalText = $('#input').val();
+        piglatinizer(originalText);
+    })
+})
